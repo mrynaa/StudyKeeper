9979745c0b9c6713f8580f2d70ca289ce9040a2d
mrynaa <103993473+mrynaa@users.noreply.github.com>
2022-05-17 19:43:55 +0200

    Update summary.php

--- summary.php
+++ summary.php
@@ -1,52 +1,45 @@
-<?php
-    session_start();
-?>
-
-
-<form name="loginForm" action="session.php" class="summary" method="post">
-    <h2> Studio terminato </h2>
+<form name="loginForm" action="" class="summary" method="post" onsubmit="remember()">
+    <h2 style="font-family:'Times New Roman', Times, serif;"> Complimenti, la tua sessione di studio è terminata! </h2>
+    <p></p>
+    <caption><i>Riepilogo</i></caption>
     <table class="table">
-        <caption> Riepilogo </caption>
         <tbody>
             <tr>
-                <td> Tempo di studio </td>
-                <td name="time"> <?= $_POST["studyTime"] ?> </td>
+                <td> Tempo di studio: </td>
+                <td> <?= $_POST["studyTime"] ?> </td>
             </tr>
             <tr>
-                <td> Pause </td>
+                <td>Numero delle pause:</td>
                 <td> <?= $_POST["pause"] ?> </td>
             </tr>
         </tbody>
     </table> 
-
-
-    <input type="hidden" name="studyTime" value="<?= $_POST["studyTime"] ?>">
-    <input type="hidden" name="pause" value="<?= $_POST["pause"] ?>">
-    <label style="font-size: 20px;"> Dai un voto a questa sessione* </label> <br>
+    <label> Dai un voto a questa sessione*: </label> <br>
     <div class="form-check checkbox-inline">
-        <input class="form-check-input" type="radio" name="valutation" id="voto1" value="1">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="voto1" value="1" required>
         <label class="form-check-label" for="inlineRadio1"> ★ </label>
     </div>
     <div class="form-check checkbox-inline">
-        <input class="form-check-input" type="radio" name="valutation" id="voto2" value="2">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="voto2" value="2">
         <label class="form-check-label" for="inlineRadio3"> ★★ </label>
     </div>
     <div class="form-check checkbox-inline">
-        <input class="form-check-input" type="radio" name="valutation" id="voto3" value="3">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="voto3" value="3">
         <label class="form-check-label" for="inlineRadio3"> ★★★	 </label>
     </div>
     <div class="form-check checkbox-inline">
-        <input class="form-check-input" type="radio" name="valutation" id="voto4" value="4">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="voto4" value="4">
         <label class="form-check-label" for="inlineRadio3"> ★★★★ </label>
     </div>
     <div class="form-check checkbox-inline">
-        <input class="form-check-input" type="radio" name="valutation" id="voto5" value="5">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="voto5" value="5">
         <label class="form-check-label" for="inlineRadio3"> ★★★★★ </label>
     </div> <br> <br>
     <div>
-        <label class="form-label" for="comm"> Lascia un commento </label>
-        <textarea class="form-control" maxlenght="200" name="comment" id="comm" rows="4"> </textarea>
+        <label class="form-label" for="comm"> Lascia un commento: </label>
+        <textarea class="form-control" id="comm" rows="4"> </textarea>
     </div> <br>
     <button type="submit" class="btn btn-warning button-ss" name="submitButton"> Invia </button>
-    <a href="" style="text-decoration: none; float: right;" class="linknav"> Indietro </a>
+    <button type="reset" class="btn btn-warning button-ss" name="resetButton"> Reset </button>
+    <a href="" style="text-decoration:none; float: right;" class="linknav"> Torna indietro </a>
 </form>
