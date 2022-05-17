476fdc3dc0b3c8ed69239dc5523f6fd819c13ab4
LafaDR-23 <rdarida32@gmail.com>
2022-05-18 00:31:39 +0200

    Add files via upload

---
+++ AccessoTimerStatistiche/delconfirm.php
@@ -0,0 +1,17 @@
+<div class="main-body">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="d-flex align-items-center mb-3">Stai per cancellare il tuo profilo. Di conseguenza tutti i tuoi dati e le tue sessioni di studio verrano rimosse dal sito. Desideri continuare?</h5>
+					<p></p>
+                </div>
+			</div>
+        </div>	
+	</div>
+</div>
+<div id="d_button" class="gap-3 d-md-flex justify-content-md-end text-center">
+    <button type="submit" name="del_button" class="btn btn-warning button-ss"> Si </button>
+    <p></p>
+    <a href=profilo.php style="text-decoration: none;"> <button type="button" class="btn btn-warning button-ss"> No </button> </a>
+</div>
---
+++ AccessoTimerStatistiche/modify.php
@@ -0,0 +1,60 @@
+<?php
+    session_start();
+    if(isset($_POST["del_button"])) {
+        $dbconn = pg_connect("host=localhost dbname=StudyKeeper password=superuser user=postgres port=5432")
+                or die(pg_last_error());
+        $query = "SELECT idu from utente where username = '".$_SESSION["userSession"]."'";
+        $result = pg_query_params($dbconn, $query, array());
+        $row = pg_fetch_array($result, null, PGSQL_ASSOC);
+        $query = "DELETE from sessione where username = '".$row["idu"]."'";
+        $query2 = "DELETE from utente where idu = '".$row["idu"]."'";
+        $result = pg_query_params($dbconn, $query, array());
+        if($result) {
+            $result = pg_query_params($dbconn, $query2, array());
+            if($result) {
+                $_SESSION["canc_confirm"] = "";
+                setcookie("LoggedUser", "CANC", time() - 3600);
+                header("Location: http://localhost:3000/registrazione.php");
+            }
+        }  
+    } 
+    if(isset($_POST["save_button"]))  {
+    $dbconn = pg_connect("host=localhost dbname=StudyKeeper password=superuser user=postgres port=5432")
+                or die(pg_last_error());
+    if($_POST["new_username"]) {
+        $query = "UPDATE utente
+                    set username = '".$_POST["new_username"]."'
+                where username = '".$_SESSION["userSession"]."'";
+        $result = pg_query_params($dbconn, $query, array());
+        setcookie("LoggedUser", "CANC", time() - 3600);
+        setcookie("LoggedUser", $_POST["new_username"], time() + (86400*30));
+        $_SESSION["userSession"] = $_POST["new_username"];
+    } 
+
+    if($_POST["new_email"]) {
+        $query = "UPDATE utente 
+                    set email = '".$_POST["new_email"]."'
+                where username = '".$_SESSION["userSession"]."'";
+        $result = pg_query_params($dbconn, $query, array());
+    }
+
+    if($_POST["new_old"]) {
+        $query = "UPDATE utente 
+                    set età = '".$_POST["new_old"]."'
+                where username = '".$_SESSION["userSession"]."'";
+        $result = pg_query_params($dbconn, $query, array());
+    }
+
+    if($_POST["new_rank"]) {
+        $query = "UPDATE utente 
+                    set grado = '".$_POST["new_rank"]."'
+                where username = '".$_SESSION["userSession"]."'";
+        $result = pg_query_params($dbconn, $query, array());
+    }
+
+    header("Location: http://localhost:3000/profilo.php");
+
+    } else {
+        echo "ERRORE 404: Pagina non trovata";
+    }
+?>
