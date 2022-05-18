ad59b24904352666498f428f1498f14a1e12133b
mrynaa <103993473+mrynaa@users.noreply.github.com>
2022-05-18 17:39:24 +0200

    Update reg.php

--- FileCompleti/reg.php
+++ FileCompleti/reg.php
@@ -3,22 +3,27 @@
     $dbconn = pg_connect("host=localhost dbname=StudyKeeper password=superuser user=postgres port=5432")
                 or die(pg_last_error());
     $user = $_POST["username"];
+    $email = $_POST["email"];
     $query = 'SELECT * from utente where username = $1';
     $result = pg_query_params($dbconn, $query, array($user));
     if(pg_fetch_array($result, null, PGSQL_ASSOC)) {
         $_SESSION["key_error"] = "";
         header("Location: http://localhost:3000/registrazione.php");
     } else {
-        $email = $_POST["email"];
-        $psw = $_POST["password"];
-        $iscrizione = date('Y-m-d');
-        $query = 'INSERT into utente values ($1,$2,$3,$4)';
-        $result = pg_query_params($dbconn, $query, array($user, $email, $psw, $iscrizione));
-        if($result) {
-            $_SESSION["succ_reg"] = "";
-            header("Location: http://localhost:3000/login.php");
+        $query = 'SELECT * from utente where email = $1';
+        $result = pg_query_params($dbconn, $query, array($email));
+        if(pg_fetch_array($result, null, PGSQL_ASSOC)) {
+            $_SESSION["email_error"] = "";
+            header("Location: http://localhost:3000/registrazione.php");
+        } else {
+            $psw = $_POST["password"];
+            $query = 'INSERT into utente values (DEFAULT,$1,$2,$3,CURRENT_DATE)';
+            $result = pg_query_params($dbconn, $query, array($user, $email, $psw));
+            if($result) {
+                $_SESSION["succ_reg"] = "";
+                header("Location: http://localhost:3000/login.php");
+            }
         }
     }
     pg_close($dbconn);
-
 ?>
