ca06e6ecb9a121e3475d41f6d50bcbaeccb3d8e8
mrynaa <103993473+mrynaa@users.noreply.github.com>
2022-06-03 11:58:00 +0200

    Delete Accesso directory

--- Accesso/main.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    if(!isset($_COOKIE["LoggedUser"])) {
-        header("Location: http://localhost:3000/login.php");
-    } else {
-        echo "Welcome" . $_COOKIE["LoggedUser"];
-    }
-?>
